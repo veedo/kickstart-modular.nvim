7fb0e541d206d2f341570767581253448aea6a61
Zander Erasmus <zerasmus@corvusenergy.com>
2024-03-04 21:41:53 -0800

    Undo files in separate dir. Convert tabs to spaces.

--- lua/options.lua
+++ lua/options.lua
@@ -75,4 +75,14 @@ vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 
+-- Use spaces for tabs, and default to 4
+vim.opt.tabstop = 8
+-- might need this to convert tabs to spaces:
+-- vim.opt.softtabstop = 0
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+
 -- vim: ts=2 sts=2 sw=2 et
