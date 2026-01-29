cb46abced19cf0d9e358deb238942473469451ce
Zander Erasmus <zerasmus@corvusenergy.com>
2026-01-29 12:23:38 -0800

    Include markdown renderer

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,6 +23,7 @@ require('lazy').setup({
 
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
+  require 'kickstart/plugins/render-markdown',
 
   require 'kickstart/plugins/gitsigns',
 
