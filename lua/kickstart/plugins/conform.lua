d55b3b4935ea3617fab477651fbe705ced7630dc
Zander Erasmus <zerasmus@corvusenergy.com>
2026-03-09 21:36:03 -0700

    Auto format can sometimes break shiz

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,16 +1,15 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
     keys = {
       {
+        -- Customize or remove this keymap to your liking
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require('conform').format { async = true, lsp_fallback = true }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = 'Format buffer',
       },
     },
     opts = {
