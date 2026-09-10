6ab8ccb9f940770822c0ce8777cf1be8f686e710
Zander Erasmus <zerasmus@corvusenergy.com>
2026-09-10 15:01:34 -0700

    Wire up vim-fugitive for :G/:Git
    Was added to the repo long ago (2458f7b) in old lazy.nvim spec format but
    never required from plugins.lua, so it was never actually installed/loaded.

--- lua/kickstart/plugins/fugitive.lua
+++ lua/kickstart/plugins/fugitive.lua
@@ -1,1 +1,5 @@
-return { { 'tpope/vim-fugitive' } }
+-- [[ Git Integration ]]
+-- vim-fugitive: `:G` / `:Git` and friends
+-- vim-fugitive is provided as a Nix-managed native package (see modules/packages/nvim/neovim.nix)
+
+-- vim: ts=2 sts=2 sw=2 et
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,7 @@
 
 require 'kickstart.plugins.guess-indent'
 require 'kickstart.plugins.gitsigns'
+require 'kickstart.plugins.fugitive'
 require 'kickstart.plugins.which-key'
 require 'kickstart.plugins.tokyonight'
 require 'kickstart.plugins.todo-comments'
